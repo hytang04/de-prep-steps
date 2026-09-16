c2f4957fdd0b34afe564189b226e493812e0487a
Hao-Yen Tang <haoyen.tang@neptunenorth.co.uk>
2026-09-16 16:16:23 +0100

    Update sets and tuples folder

--- 14_sets_and_tuples/01_sets.py
+++ 14_sets_and_tuples/01_sets.py
@@ -12,7 +12,7 @@ def create_set(numbers):
     """
     This function should take a list of numbers and convert it to a set
     """
-    pass
+    return set(numbers)
 
 
 @run_test
@@ -36,10 +36,10 @@ def is_in_set(my_set, element):
     This function should take a set and an element and return True if the set
     contains the element and False otherwise
     """
-    pass
+    return element in my_set
 
 
-@skip_test
+@run_test
 def test_is_in_set():
     assert is_in_set({5, 2, 3, 4}, 1) is False, \
         format_err_msg(False, is_in_set({5, 2, 3, 4}, 1))
@@ -54,10 +54,10 @@ def remove_set_element(my_set):
     This function should take a set, remove any one element from it and return
     the removed element
     """
-    pass
+    return my_set.pop()
 
 
-@skip_test
+@run_test
 def test_remove_set_element():
     test_set = {1, 2, 3, 4, 5}
     removed_element = remove_set_element(test_set)
@@ -76,10 +76,11 @@ def discard_set_element(my_set, value):
     This function should take a set and a value. It should remove the
     specified value from the set and return the original set.
     """
-    pass
+    my_set.discard(value)
+    return my_set
 
 
-@skip_test
+@run_test
 def test_discard_set_element():
     assert discard_set_element(
         {"help", "fix", "my", "code"}, "help") == {"fix", "my", "code"}, \
@@ -97,10 +98,10 @@ def copy_set(my_set):
     """
     This function should take a set and return a *new copy* of that set
     """
-    pass
+    return my_set.copy()
 
 
-@skip_test
+@run_test
 def test_copy_set():
     assert copy_set({1, 2, 3}) == {1, 2, 3}, \
         format_err_msg({1, 2, 3}, copy_set({1, 2, 3}))
--- 14_sets_and_tuples/tuples.py
+++ 14_sets_and_tuples/tuples.py
@@ -13,7 +13,7 @@ def create_tuple(arg_1, arg_2):
     This function takes two arguments and should return a tuple
         containing the arguments
     """
-    pass
+    return (arg_1, arg_2)
 
 
 @run_test
@@ -34,10 +34,10 @@ def tuple_to_list(my_tuple):
     """
     This function should convert the given tuple to a list
     """
-    pass
+    return list(my_tuple)
 
 
-@skip_test
+@run_test
 def test_tuple_to_list():
     assert tuple_to_list(("Alex", "Windows")) == ["Alex", "Windows"], \
         format_err_msg(["Alex", "Windows"], tuple_to_list(("Alex", "Windows")))
@@ -55,10 +55,10 @@ def count_threes(my_tuple):
     This function should return the number of times the string "three" appears
         in the given tuple
     """
-    pass
+    return my_tuple.count("three")
 
 
-@skip_test
+@run_test
 def test_count_threes():
     assert count_threes((1, 2, 3, 4, 5)) == 0, \
         format_err_msg(0, count_threes((1, 2, 3, 4, 5)))
@@ -76,10 +76,10 @@ def get_index_of_five(my_tuple):
     """
     This function takes a tuple and should return the index of the number 5
     """
-    pass
+    return my_tuple.index(5)
 
 
-@skip_test
+@run_test
 def test_get_index_of_five():
     assert get_index_of_five((1, 6, 5, 2, 5)) == 2, \
         format_err_msg(2, get_index_of_five((1, 6, 5, 2, 5)))
@@ -95,10 +95,10 @@ def get_second_to_last_element(my_tuple):
     """
     This function takes a tuple and should return the 2nd to last element
     """
-    pass
+    return my_tuple[-2]
 
 
-@skip_test
+@run_test
 def test_get_second_to_last_element():
     assert get_second_to_last_element((1, 6, 5, 2, 5)) == 2, \
         format_err_msg(2, get_second_to_last_element((1, 6, 5, 2, 5)))
@@ -119,10 +119,10 @@ def get_last_three_elements(my_tuple):
     """
     This function should return the final 3 elements of the given tuple
     """
-    pass
+    return my_tuple[-3:]
 
 
-@skip_test
+@run_test
 def test_get_last_three_elements():
     assert get_last_three_elements((1, 6, 5, 2, 5)) == (5, 2, 5), \
         format_err_msg((5, 2, 5), get_last_three_elements((1, 6, 5, 2, 5)))
@@ -150,10 +150,10 @@ def check_element_is_present(my_tuple, element):
     True - if the element is present in the tuple
     False - if the element is NOT present in the tuple
     """
-    pass
+    return element in my_tuple
 
 
-@skip_test
+@run_test
 def test_check_element_is_present():
     assert check_element_is_present((1, 6, 5, 2, 5), 6) is True, \
         format_err_msg(True,
@@ -189,10 +189,10 @@ def tuple_switcheroo(*args):
 
     E.g: tuple_switcheroo(1,2,3) should return (3,2,1)
     """
-    pass
+    return tuple(reversed(args))
 
 
-@skip_test
+@run_test
 def test_tuple_switcheroo():
     assert tuple_switcheroo(3, 4) == (4, 3), \
         format_err_msg((4, 3), tuple_switcheroo(3, 4))
