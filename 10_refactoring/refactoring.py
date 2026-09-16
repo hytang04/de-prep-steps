1e87f9f048ef0a478dc664126f4b6fdff0b6e6fc
Hao-Yen Tang <haoyen.tang@neptunenorth.co.uk>
2026-09-16 14:56:21 +0100

    Update refactoring folder

--- 10_refactoring/refactoring.py
+++ 10_refactoring/refactoring.py
@@ -25,10 +25,7 @@ If you run python on this file, all tests should pass.
 
 
 def triple_nums(nums):
-    tripled = []
-    for i in range(len(nums)):
-        tripled.append(3 * nums[i])
-    return tripled
+    return [3 * num for num in nums]
 
 
 # Do not change tests!
@@ -69,30 +66,27 @@ If you run python on this file, all tests should pass.
 
 
 def shout_names(names):
-    shouted_names = []
-    for name in names:
-        shouted_names.append(name.upper() + "!")
-    return shouted_names
+    return [name.upper() + "!" for name in names]
 
 
 # Do not change tests!
 
 
-@skip_test
+@run_test
 def shout_names_should_return_a_list():
     result = shout_names([])
     expected = []
     assert isinstance(result, list), format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def shout_names_should_return_empty_list_when_passed_empty_list():
     result = shout_names([])
     expected = []
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def shout_names_should_shout_names():
     result = shout_names(["Carrie", "Diya", "Kyle", "Christian"])
     expected = ["CARRIE!", "DIYA!", "KYLE!", "CHRISTIAN!"]
@@ -116,16 +110,13 @@ If you run python on this file, all tests should pass.
 
 
 def is_sweet_enough(food_items):
-    for i in range(len(food_items)):
-        if food_items[i]["flavour"] != "sweet":
-            return False
-    return True
+    return all(food["flavour"] == "sweet" for food in food_items)
 
 
 # Do not change tests!
 
 
-@skip_test
+@run_test
 def is_sweet_enough_should_return_true_when_all_foods_are_sweet():
     result = is_sweet_enough(
         [
@@ -138,7 +129,7 @@ def is_sweet_enough_should_return_true_when_all_foods_are_sweet():
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def is_sweet_enough_should_return_false_when_no_foods_are_sweet():
     result = is_sweet_enough(
         [
@@ -151,7 +142,7 @@ def is_sweet_enough_should_return_false_when_no_foods_are_sweet():
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def is_sweet_enough_should_return_false_when_any_foods_are_not_sweet():
     result = is_sweet_enough(
         [
@@ -178,37 +169,31 @@ If you run python on this file, all tests should pass.
 
 
 def get_excited(text):
-    new_text = ""
-    for char in text:
-        if char == ".":
-            new_text += "!"
-        else:
-            new_text += char
-    return new_text
+    return text.replace(".", "!")
 
 
-@skip_test
+@run_test
 def get_excited_should_return_empty_string_when_passed_empty_string():
     result = get_excited("")
     expected = ""
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_excited_should_return_unchanged_string_when_no_full_stops_present():
     result = get_excited("the quick brown fox jumps over the lazy dog")
     expected = "the quick brown fox jumps over the lazy dog"
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_excited_should_replace_full_stops_with_exclamation_marks_single_full_stop():
     result = get_excited("We're gonna need a bigger boat.")
     expected = "We're gonna need a bigger boat!"
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def get_excited_should_replace_full_stops_with_exclamation_marks_multiple_full_stops():
     result = get_excited("Woo. Woo. Woo. Who's ready to code?")
     expected = "Woo! Woo! Woo! Who's ready to code?"
@@ -228,24 +213,20 @@ If you run python on this file, all tests should pass.
 
 
 def shrek_characters(characters):
-    shreks = []
-    for character in characters:
-        if "Shrek" in character["movie"]:
-            shreks.append(character["name"])
-    return shreks
+    return [character["name"] for character in characters if character["movie"] == "Shrek"]
 
 
 # Do not change tests!
 
 
-@skip_test
+@run_test
 def shrek_characters_should_return_empty_list_when_passed_empty_list():
     result = shrek_characters([])
     expected = []
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def shrek_characters_should_return_empty_list_when_no_shrek_characters():
     result = shrek_characters(
         [
@@ -258,7 +239,7 @@ def shrek_characters_should_return_empty_list_when_no_shrek_characters():
     assert result == expected, format_err_msg(expected, result)
 
 
-@skip_test
+@run_test
 def shrek_characters_should_return_list_of_shrek_characters():
     result = shrek_characters(
         [
